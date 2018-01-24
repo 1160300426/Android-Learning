80f45ad5f4376e75d89c071e5c209239314c7648
FightingStone <13144602916@163.com>
2018-01-24 22:24:55 +0800

    Android Intent

---
+++ ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,96 @@
+package com.example.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    //@Override表示下面的方法继承于父类.
+
+    //重写方法，定义菜单响应事件.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity",returnedData);
+                }
+                break;
+            default:
+        }
+    }
+
+    //重写方法，创建菜单.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                //发送短小的消息给用户.
+//                Toast.makeText(FirstActivity.this,"You clicked Button 1",Toast.LENGTH_SHORT).show();
+
+//                //显示Intent.
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                startActivity(intent);
+
+//                //隐示Intent.
+//                Intent intent = new Intent("com.example.activitytest.ACTION_START");
+//                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+//                startActivity(intent);
+
+//                //隐示2.
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent);
+
+//                //隐示3.
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+
+//                //传递数据.
+//                String data = "Hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                startActivity(intent);
+
+                //返回数据给上一个活动.
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+}
